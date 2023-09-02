564503ee36ae2399f73ea438c2289c270de7a444
alifakbarr <alifakbar8800@gmail.com>
2023-09-02 18:25:10 +0700

    fix: template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+<aside class="aside is-placed-left is-expanded">
+    <div class="aside-tools">
+      <div>
+        Admin <b class="font-black">alifakbar</b>
+      </div>
+    </div>
+    <div class="menu is-menu-main">
+      <p class="menu-label">General</p>
+      <ul class="menu-list">
+        <li class="active">
+          <a href="index.html">
+            <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+            <span class="menu-item-label">Dashboard</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </aside>
+  
